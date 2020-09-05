5c80b2c6b0bca13d42f8e6981c32660494ba88f9
Afonso -aptsharp <aff-on@hotmail.com>
2020-09-05 15:04:44 -0300

    Criação de estrutura

---
+++ devboost.Domain/Handles/Commands/ClientePayAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using devboost.Domain.Handles.Commands.Interfaces;
+
+namespace devboost.Domain.Handles.Commands
+{
+    public class ClientePayAPI : IClientPayAPI
+    {
+
+    }
+}
---
+++ devboost.Domain/Handles/Commands/Interfaces/IClientPayAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace devboost.Domain.Handles.Commands.Interfaces
+{
+    public interface IClientPayAPI
+    {
+
+
+
+
+    }
+}
