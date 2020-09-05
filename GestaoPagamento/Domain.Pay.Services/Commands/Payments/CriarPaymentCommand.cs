1822177fb788faabfa059a4075bb9df7101576ac
allansud <freitasallan@gmail.com>
2020-09-05 15:03:55 -0300

    Payment

---
+++ GestaoPagamento/Domain.Pay.Services/Commands/Payments/CriarPaymentCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Domain.Pay.Services.Commands.Payments
+{
+    public class CriarPaymentCommand
+    {
+        public DateTime CreatedAt { get; }
+
+        public string Name { get; private set; }
+
+        public string Bandeira { get; }
+
+        public string NumeroCartao { get; }
+
+        public DateTime Vencimento { get; }
+
+        public int CodigoSeguranca { get; }
+
+        public double Valor { get; }
+
+        public string Status { get; }
+
+        public CriarPaymentCommand(DateTime createdAt, string name, string bandeira)
+        {
+
+        }
+    }
+}
