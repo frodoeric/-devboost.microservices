de33a29818118716b9a96a69a7f7992d94a36235
allansud <freitasallan@gmail.com>
2020-09-05 14:24:52 -0300

    Model...

---
+++ GestaoPagamento/Pay.API/Controllers/PaymentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pay.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+
+    }
+}
