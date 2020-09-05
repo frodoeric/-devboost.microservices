35f4c6c53a34836d19b66e071d7c464b315002f4
Afonso -aptsharp <aff-on@hotmail.com>
2020-09-05 16:04:09 -0300

    Finalizacao metodo realizarpamaento

--- devboost.Domain/Handles/Commands/Interfaces/IPayAPIHandler.cs
+++ devboost.Domain/Handles/Commands/Interfaces/IPayAPIHandler.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace devboost.Domain.Handles.Commands.Interfaces
 {
     public interface IPayAPIHandler
     {
-
-
-
+        public Task<HttpResponseMessage> PostRealizarPagamento();
 
     }
 }
--- devboost.Domain/Handles/Commands/PayAPIHandler.cs
+++ devboost.Domain/Handles/Commands/PayAPIHandler.cs
@@ -5,21 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using devboost.Domain.Commands.Request;
 using devboost.Domain.Handles.Commands.Interfaces;
+using Newtonsoft.Json;
 
 namespace devboost.Domain.Handles.Commands
 {
     public class PayAPIHandler : IPayAPIHandler
     {
         private readonly HttpClient httpClient;
+        string uri = "https://localhost:44339/api/payment/pagamento";
 
         public PayAPIHandler(HttpClient httpClient)
         {
             this.httpClient = httpClient;
         }
 
-        public async Task<RealizarPedidoRequest> PostRealizarPedido()
+        public async Task<HttpResponseMessage> PostRealizarPagamento()
         {
-            return null;
+            CmmPagRequest realizarPagamentoRequest = new CmmPagRequest();
+
+            StringContent pedidoJson = new StringContent(JsonConvert.SerializeObject(realizarPagamentoRequest));
+
+            var result = await httpClient.PostAsync(uri, pedidoJson);
+            return result;
         }
 
     }
