ad8bea02626924d059e30e03f4acffaa7dbfb224
Afonso -aptsharp <aff-on@hotmail.com>
2020-09-08 19:59:52 -0300

    Criação de testes CriarPaymentHandlerTest.cs

---
+++ GestaoPagamento/Tests.Pay.TDD/Repository/CriarPaymentHandlerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Pay.TDD.Repository
+{
+    public class CriarPaymentHandlerTest
+    {
+
+
+        public CriarPaymentHandlerTest()
+        {
+
+        }
+
+
+
+       
+
+
+
+    }
+}
