442709d3b46ecb1911d5b4fbadaff1ba02293930
allansud <freitasallan@gmail.com>
2020-09-08 22:00:06 -0300

    CommandPaymentHandlerSteps.cs

--- GestaoPagamento/Tests.Pay.BDD/Steps/Command/CommandPaymentHandlerSteps.cs
+++ GestaoPagamento/Tests.Pay.BDD/Steps/Command/CommandPaymentHandlerSteps.cs
@@ -1,6 +1,9 @@
 ﻿using Domain.Pay.Services.CommandHandlers.Interfaces;
 using Domain.Pay.Services.Commands.Payments;
+using Domain.Pay.Services.Queries.Payments;
+using Domain.Pay.Services.QueryHandler.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using TechTalk.SpecFlow;
 using Tests.Pay.TDD.Config;
 
@@ -11,21 +14,37 @@ namespace Tests.Pay.BDD.Steps.Command
     {
         readonly ScenarioContext _context;
         readonly ICriarPaymentHandler _criarPaymentHandler;
+        readonly IListarPaymentsHandler _listarPaymentsHandler;
 
-        public CommandPaymentHandlerSteps(ScenarioContext context, ICriarPaymentHandler criarPaymentHandler)
+        public CommandPaymentHandlerSteps(ScenarioContext context)
         {
             var _serviceProvider = new StartInjection().ServiceProvider;
             _context = context;
-            _criarPaymentHandler = _serviceProvider.GetServices<ICriarPaymentHandler>();
+            _criarPaymentHandler = _serviceProvider.GetService<ICriarPaymentHandler>();
+            _listarPaymentsHandler = _serviceProvider.GetService<IListarPaymentsHandler>();
+
         }
 
         [Given(@"Nao Exista Payments cadastrados")]
         public void GivenNaoExistaPaymentsCadastrados()
         {
-            _criarPaymentHandler.Handle(new CriarPaymentCommand 
+            CriarPaymentCommand criarPayment = new CriarPaymentCommand
             {
+                PayId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                CreatedAt = DateTime.Parse("2020-09-07T21:19:50.304Z"),
+                Name = "Eric",
+                Bandeira = "Mastercard",
+                NumeroCartao = "1111111111111111",
+                Vencimento = DateTime.Parse("2020-09-07T21:19:50.304Z"),
+                CodigoSeguranca = 145,
+                Valor = 40,
+                Status = 1
+            };
 
-            });
+            _criarPaymentHandler.Handle(criarPayment);
+            var payment = _listarPaymentsHandler.Handle(new PaymentsQuery());
+
+            _context.Set(payment);
         }
     }
 }
