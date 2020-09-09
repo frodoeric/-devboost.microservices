fad58c48bec7512dbc3ff6608534fe537c451b33
Eric Joseph Bezerra Costa <frodoeric@hotmail.com>
2020-09-08 21:42:35 -0300

    inclusão método de teste listar payments handler

--- GestaoPagamento/Domain.Pay.Services/AutoMapper/MappingProfile.cs
+++ GestaoPagamento/Domain.Pay.Services/AutoMapper/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Domain.Pay.Entities;
 using Domain.Pay.Services.Commands.Payments;
+using Domain.Pay.Services.Dtos.Payments;
 
 namespace Domain.Pay.Services.AutoMapper
 {
@@ -11,6 +12,9 @@ namespace Domain.Pay.Services.AutoMapper
             //Command
             CreateMap<CriarPaymentCommand, Payment>()
                 .ConstructUsing(c => Payment.Criar(c.PayId, c.CreatedAt, c.Name, c.Bandeira, c.NumeroCartao, c.Vencimento, c.CodigoSeguranca, c.Valor, c.Status));
+
+            CreateMap<Payment, PaymentDto>();
+            CreateMap<PaymentDto, Payment>();
         }
     }
 }
